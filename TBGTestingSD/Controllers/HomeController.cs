132b12f8ddb7e8f3e477043b8ef86578866ba721
Lutfi Fauzi <Lutfi.Fauzi@totalbp.com>
2023-07-24 11:15:17 +0700

    add feat

--- TBGTestingSD/Controllers/HomeController.cs
+++ TBGTestingSD/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace TBGTestingSD.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        //test change
     }
 }
